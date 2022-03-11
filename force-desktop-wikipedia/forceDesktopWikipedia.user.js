1e70a0a663cad19469bbc5257b31013462ca7c75
Alex DeLorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2022-03-11 06:30:39 +0000

    Create forceDesktopWikipedia.user.js

---
+++ force-desktop-wikipedia/forceDesktopWikipedia.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Force desktop Wikipedia
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Disable Wikipedia's mobile UI in favor of its desktop UI
+// @author       Alex DeLorenzo <AlexDeLorenzo.dev>
+// @match        https://*.m.wikipedia.org/*
+// @grant        none
+// @license      GPL-3.0
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const MOBILE_SUB = ".m.";
+    const DOT = '.';
+
+    const current = new URL(location);
+    current.host = current.host.replace(MOBILE_SUB, DOT);
+
+    location.replace(current);
+})();
